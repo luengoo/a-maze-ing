d195bebabe23d887ba419f095d52a46879e732c8
Zack <girardzack04@example.com>
2026-03-25 18:09:23 +0100

    menu

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -2,7 +2,37 @@ from config_checker import ConfigChecker
 from maze_algorithm import generate_maze, print_maze
 import os
 
-if __name__ == "__main__":
+
+def menu(config):
+       
+    print("\n---------------MENU------------------")
+    print("1 - Regenerate a maze | 2 - Change colors | 3 - Hide path | 4 - Change color 42")
+
+    option = int(input("Enter option: "))
+
+    if option is 1:
+        grid, entry, exit = generate_maze(config)
+        os.system("cls" if os.name == "nt" else "clear")
+        print_maze(grid, entry, exit)
+
+    elif option is 2:
+
+        # maze color
+        pass
+    
+    elif option is 3:
+
+        # path toggle
+        pass
+    
+    elif option is 4:
+
+        # change color 
+        pass
+    
+
+
+def main():
     checker = ConfigChecker()
     config = checker.opener()
 
@@ -10,3 +40,9 @@ if __name__ == "__main__":
 
     os.system("cls" if os.name == "nt" else "clear")
     print_maze(grid, entry, exit)
+    while True:
+        menu(config)
+
+
+if __name__ == "__main__":
+    main()
